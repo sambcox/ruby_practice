d5941307079100b7a2fc105305b661c93b9c449b
Samuel Cox <samc1253@gmail.com>
2022-09-14 09:36:51 -0600

    Add 4 lines to test methods

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,11 @@ manufacturers = ["Ferrari", "Mercedes", "McLaren", "Red Bull"]
 championships = [27, 8, 15, 4]
 top_speed = [232.58, 215.62, 229.23, 221.87]
 is_active = [true, true, true, true]
+#next line will remove "Red Bull" from the manufacturers array
+manufacturers.pop
+#next line will add false to the end of the is_active array
+is_active.push(false)
+#next line will remove 232.58 from the top top_speed array
+top_speed.shift
+#next line will add 21 to the beginning of the championships array
+championships.unshift(21)
